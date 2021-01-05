8bbe2f7dc963f9cbcfe11f814429e64859ee4db9
Nabil <nkoney14@outlook.com>
2021-01-04 20:53:47 -0500

    made the header with the add button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <header className="bg-white
+      m-auto p-3 text-center">
+        <div className="d-flex justify-content-center">
+          <h1 className="flex-fill">Hello World</h1>
+          <button type="button" className="btn btn-outline-dark rounded-circle m-auto">+</button>
+        </div>
+      </header>
       
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
