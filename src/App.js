127109cc27b80a617053834be7064fea52ff7694
Nabil <nkoney14@outlook.com>
2021-01-04 21:17:34 -0500

    made a little blink animation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export class App extends Component {
       <header className="bg-white
       m-auto p-3 text-center">
         <div className="d-flex justify-content-center">
-          <h1 className="flex-fill">Hello World</h1>
+          <h1 className="flex-fill">Hello World<span className="animation-blink">_</span></h1>
           <button type="button" className="btn btn-outline-dark rounded-circle m-auto">+</button>
         </div>
       </header>
